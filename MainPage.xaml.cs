4c9c26de039b25b0e89ec73e69d3b9c6c8f7de20
KamilSzat <uT_3776@zsp3zamosc.pl>
2025-05-19 12:33:51 +0200

    Rejestracja użytkownika

---
+++ MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Maui.Controls.PlatformConfiguration.AndroidSpecific;
+
+namespace rejestracja
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnRegisterClicked(object sender, EventArgs e)
+        {
+            string email = emailEnrty.Text;
+            string password = haslo.Text;
+            string repairPassword = ponowne_haslo.Text;
+
+            if (string.IsNullOrWhiteSpace(email) || !email.Contains("0"))
+            {
+                wiadomosc.Text = "Nieprawidłowy adres email";
+                return;
+
+            }
+            if (password != repairPassword)
+            {
+                wiadomosc.Text = "Hasła się różnią";
+                return;
+            }
+            wiadomosc.Text = $"Witaj {email}";
+        }
+
+    }
+}
